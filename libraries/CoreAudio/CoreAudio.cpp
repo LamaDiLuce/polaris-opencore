732fafa0d0041bb81069722caf25766a92870d98
Jason Ramboz <jramboz@gmail.com>
2022-12-20 14:36:07 -0800

    Reset to max volume on next arm after mute

--- libraries/CoreAudio/CoreAudio.cpp
+++ libraries/CoreAudio/CoreAudio.cpp
@@ -85,6 +85,10 @@ void CoreAudio::action( int id ) {
         } 
       return;
     case EXT_IDLE:
+      if (currentVolume == 0) // If saber was muted on last disarm, set volume to max
+      {
+        currentVolume = MAX_VOLUME;
+      }
       return;
     case ENT_VOLUME:
       currentVolume += (MAX_VOLUME * 0.25); // Volume increases in 25% increments, giving 4 levels (25%, 50%, 75%, 100%) plus mute
